5bc2c937a734710023e65b75cb6c11e7a7db3dc5
Michael Owolabi <michael.i.owolabi@gmail.com>
2019-11-20 23:23:20 +0100

    implement system info cli tool

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# sys-info-cli
-A simple cli tool that gives basic information about your computer built with dart
+A sample command-line application.
+
+Created from templates made available by Stagehand under a BSD-style
+[license](https://github.com/dart-lang/stagehand/blob/master/LICENSE).
---
+++ bin/main.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+main() {
+  final envVarMap = Platform.environment;
+  const thumbsUp = '\u{1F44D}';
+  
+  print('\nBelow is the basic details about your computer:');
+  print('\nPWD = ${envVarMap["PWD"]}');
+  print('\nSystem name = ${envVarMap["LOGNAME"]}');
+  print('\nDart PATH = ${envVarMap["PATH"]}');
+  print('\nYou\'re running ${Platform.operatingSystem} ${Platform.operatingSystemVersion} with ${Platform.numberOfProcessors} processors');
+  print('\nYour language and current location is: ${Platform.localeName}');
+  print('\n Thank You ${thumbsUp}\n');
+}
